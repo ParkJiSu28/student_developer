a8db64e8fed0d5d6f6a04ca1acedc96f671f440a
ParkJiSu28 <qkrwltn28@gmail.com>
2018-02-12 16:47:27 +0900

    pystargram tutorial anywhere

--- pystargram1/pystargram1/settings.py
+++ pystargram1/pystargram1/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*r5k@hg$+4d^06d1tz5uujavqco&(u7w8^^lzryr89z^9o%9m4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'qkrwltn5091.pythonanywhere.com']
 
 # Application definition
 
